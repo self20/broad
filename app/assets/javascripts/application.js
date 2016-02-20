5f4a326672c3bb5faa78a9239b5ce51c15cf9cb2
Magnus Skog <magnus.m.skog@gmail.com>
2016-02-20 17:03:11 +0100

    Removed assets and added npm build

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,37 +1,13 @@
-// DO NOT REQUIRE jQuery or jQuery-ujs in this file!
-// DO NOT REQUIRE TREE!
-
 // CRITICAL that generated/vendor-bundle must be BEFORE bootstrap-sprockets and turbolinks
 // since it is exposing jQuery and jQuery-ujs
 
 //= require generated/vendor-bundle
 //= require generated/app-bundle
-
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-// require jquery
-// require jquery_ujs
 //= require turbolinks
-//= require bootstrap/collapse
-//= require bootstrap/dropdown
-//= require bootstrap/tooltip
 //= require rails-timeago
-// require_tree .
+//= require bootstrap-sprockets
 
 Turbolinks.enableTransitionCache();
 
 jQuery.timeago.settings.allowFuture = true
-
-// bootstrap-sprockets depends on generated/vendor-bundle for jQuery.
-//= require bootstrap-sprockets
 
